1abec0d36136d6406d087b8edfd4f4931002b5fe
evileyecc <c490@hotmail.com>
2015-07-01 15:51:46 +0800

    Fix one more file
    Fix  a redirect file

--- achievement/Achievement/Controller/EmptyController.class.php
+++ achievement/Achievement/Controller/EmptyController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class EmptyController extends Controller{
     public function index(){
         //根据当前控制器名来判断要执行那个城市的操作
-        $data = array('status' => false,'errormsg'=>'Wrong Location');
+        $data = array('status' => false,'error_code'=>C('error_code.Wrong_Location'));
         $this->ajaxReturn($data);
     }
 }
